eb4685101933687f5af8f6b4a69710627efcb2df
gitdegsew <abebawdegsew@gmail.com>
2023-07-10 10:15:36 +0300

    car card

---
+++ components/CarCard.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+
+import { calculateCarRent, generateCarImageUrl } from "@utils";
+import { CarProps } from "@types";
+import CustomButton from "./CustomButton";
+import CarDetails from "./CarDetails";
+
+const CarCard = () => {
+  return (
+    <div>CarCard</div>
+  )
+}
+
+export default CarCard
