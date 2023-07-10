cdbeddbfec67b670d513d865a52adf1717fd56dd
gitdegsew <abebawdegsew@gmail.com>
2023-07-10 10:21:38 +0300

    transmission

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -34,6 +34,18 @@ const CarCard = ({ car }: CarCardProps) => {
       <div className='relative w-full h-40 my-3 object-contain'>
         <Image src={generateCarImageUrl(car)} alt='car model' fill priority className='object-contain' />
       </div>
+
+      <div className='relative flex w-full mt-2'>
+        <div className='flex group-hover:invisible w-full justify-between text-grey'>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image src='/steering-wheel.svg' width={20} height={20} alt='steering wheel' />
+            <p className='text-[14px] leading-[17px]'>
+              {transmission === "a" ? "Automatic" : "Manual"}
+            </p>
+          </div>
+
+          
+        </div>
       
     </div>
   );
