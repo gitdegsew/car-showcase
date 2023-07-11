f928236d1ac643cd52c163573495141ed3caf4ca
gitdegsew <abebawdegsew@gmail.com>
2023-07-11 10:26:42 +0300

    title more

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -59,7 +59,8 @@ const CarCard = ({ car }: CarCardProps) => {
 
         <div className="car-card__btn-container">
         <CustomButton
-            
+            title='View More'
+            containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
             textStyles='text-white text-[14px] leading-[17px] font-bold'
             rightIcon='/right-arrow.svg'
             handleClick={() => setIsOpen(true)}
