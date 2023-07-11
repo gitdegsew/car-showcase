a776e6b6575eb087847dff78120d8954b740d8a7
gitdegsew <abebawdegsew@gmail.com>
2023-07-11 10:34:24 +0300

    interface is open

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -4,6 +4,11 @@ import { Dialog, Transition } from "@headlessui/react";
 import { CarProps } from "@types";
 import { generateCarImageUrl } from "@utils";
 
+interface CarDetailsProps {
+    isOpen: boolean;
+    
+  }
+
 const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => (
   <>
     <Transition appear show={isOpen} as={Fragment}>
