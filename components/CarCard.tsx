e4affdaff26433150c8d32550cd115c0e7f1f92c
gitdegsew <abebawdegsew@gmail.com>
2023-07-11 10:23:41 +0300

    car card icon

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -48,7 +48,10 @@ const CarCard = ({ car }: CarCardProps) => {
             <Image src="/tire.svg" width={20} height={20} alt="seat" />
             <p className="car-card__icon-text">{drive.toUpperCase()}</p>
           </div>
-          
+          <div className="car-card__icon">
+            <Image src="/gas.svg" width={20} height={20} alt="seat" />
+            <p className="car-card__icon-text">{city_mpg} MPG</p>
+          </div>
 
           
         </div>
