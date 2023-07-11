71ed035d4877512119b31d1d67c6adf100a683e4
gitdegsew <abebawdegsew@gmail.com>
2023-07-11 10:33:37 +0300

    utils

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -2,7 +2,7 @@ import { Fragment } from "react";
 import Image from "next/image";
 import { Dialog, Transition } from "@headlessui/react";
 import { CarProps } from "@types";
-
+import { generateCarImageUrl } from "@utils";
 
 const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => (
   <>
