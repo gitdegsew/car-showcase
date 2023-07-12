e767c83b42e2a570882352b52421570239b265ff
gitdegsew <abebawdegsew@gmail.com>
2023-07-12 10:51:10 +0300

    changed transion durtion

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -38,6 +38,8 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => (
               leaveTo='opacity-0 scale-95'
               
             >
+              <Dialog.Panel className='relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5'>
+
               
             </Transition.Child>
           </div>
