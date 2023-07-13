ab0a5560830f46cfb788c5e556219a8ad87460a5
gitdegsew <abebawdegsew@gmail.com>
2023-07-13 11:02:28 +0300

    changed transion durtion

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -68,7 +68,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => (
                     <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
                     <Image  src={generateCarImageUrl(car, "13")} alt='car model' fill priority className='object-contain' />
                     </div>
-                    
+                    </div>
                   </div>
 
 
