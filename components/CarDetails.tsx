6ed4c841011d02e4620234e78796ed82285e43d2
gitdegsew <abebawdegsew@gmail.com>
2023-07-15 11:05:17 +0300

    changed transion durtion

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -71,8 +71,9 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => (
                     </div>
                   </div>
 
-                  
+
                 <div className='flex-1 flex flex-col gap-2'>
+                <h2 className='font-semibold text-xl capitalize'>
 
 
 
