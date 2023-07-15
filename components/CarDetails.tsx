cdc5dcab4e328616857fbe1cd1faae6b0d4fab7e
gitdegsew <abebawdegsew@gmail.com>
2023-07-15 11:08:43 +0300

    changed car details

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -83,6 +83,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => (
                         <h4 className='text-grey capitalize'>
                         {key.split("_").join(" ")}
                         </h4>
+                        <p className='text-black-100 font-semibold'>
 
 
 
