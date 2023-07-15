43039247e13f8bb77c0e54d8f08dd228dae92331
gitdegsew <abebawdegsew@gmail.com>
2023-07-15 11:06:55 +0300

    changed car details

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -77,6 +77,9 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => (
                 {car.make} {car.model}
                 </h2>
 
+                <div className='mt-3 flex flex-wrap gap-4'>
+
+
 
 
                 </div>
