2c2121d259d938f43e5dd17e5ba6e0be0d05306c
gitdegsew <abebawdegsew@gmail.com>
2023-07-16 11:20:11 +0300

    custom button

--- components/CutomButton.tsx
+++ components/CutomButton.tsx
@@ -15,4 +15,5 @@ const Button = ({ isDisabled, btnType, containerStyles, textStyles, title, right
       <div className="relative w-6 h-6">
         <Image
           src={rightIcon}
+          alt="arrow_left"
 
