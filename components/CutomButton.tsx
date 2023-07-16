bd24913c671b6a565d4e9db48a4864c3bddd1f7d
gitdegsew <abebawdegsew@gmail.com>
2023-07-16 11:17:06 +0300

    custom button

--- components/CutomButton.tsx
+++ components/CutomButton.tsx
@@ -5,3 +5,4 @@ import { CustomButtonProps } from "@types";
 
 const Button = ({ isDisabled, btnType, containerStyles, textStyles, title, rightIcon, handleClick }: CustomButtonProps) => (
     <button
+    disabled={isDisabled}
