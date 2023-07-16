9b1b39884a907cadf312f2c0388af11e49a4a680
gitdegsew <abebawdegsew@gmail.com>
2023-07-16 11:16:40 +0300

    custom button

--- components/CutomButton.tsx
+++ components/CutomButton.tsx
@@ -1,3 +1,6 @@
 "use client";
 import Image from "next/image";
 import { CustomButtonProps } from "@types";
+
+
+const Button = ({ isDisabled, btnType, containerStyles, textStyles, title, rightIcon, handleClick }: CustomButtonProps) => (
