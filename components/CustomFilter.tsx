4dfb49d446c7c8e74a755cea0a4380a84b94acdb
gitdegsew <abebawdegsew@gmail.com>
2023-07-16 11:26:33 +0300

    filte cars

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -3,3 +3,4 @@ import { Fragment, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Listbox, Transition } from "@headlessui/react";
+import { CustomFilterProps } from "@types";
