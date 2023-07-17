b02bacb10411ce415d6452be4debc758f9ba4d64
gitdegsew <abebawdegsew@gmail.com>
2023-07-17 12:27:11 +0300

    filte cars

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -5,3 +5,5 @@ import { useRouter } from "next/navigation";
 import { Listbox, Transition } from "@headlessui/react";
 import { CustomFilterProps } from "@types";
 import { updateSearchParams } from "@utils";
+
+export default function CustomFilter({ title, options }: CustomFilterProps) {
