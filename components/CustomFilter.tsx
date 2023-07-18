ddf8d070f316c2d75dbe570b148f937d50daecaf
gitdegsew <abebawdegsew@gmail.com>
2023-07-18 12:36:06 +0300

    update selected option

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -31,6 +31,8 @@ return (
           <Transition
             as={Fragment} // group multiple elements without introducing an additional DOM node i.e., <></>
             leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+
 
 
 
