b7daf6b08f8dfb4c357c8ff531ec4700b44dc85e
gitdegsew <abebawdegsew@gmail.com>
2023-07-18 12:34:39 +0300

    update selected option

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -26,6 +26,8 @@ return (
           <Listbox.Button className='custom-filter__btn'>
           <span className='block truncate'>{selected.title}</span>
           <Image src='/chevron-up-down.svg' width={20} height={20} className='ml-4 object-contain' alt='chevron_up-down' />
+          </Listbox.Button>
+
 
 
 
