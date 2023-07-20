e5ee568b9472b16f822fc9c5d7b86a67a1fb87a3
gitdegsew <abebawdegsew@gmail.com>
2023-07-20 12:40:18 +0300

    update selected option

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -43,6 +43,7 @@ return (
                   className={({ active }) =>
                   `relative cursor-default select-none py-2 px-4 ${
                     active ? "bg-primary-blue text-white" : "text-gray-900"
+                }`
 
 
 
