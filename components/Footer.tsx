662622f9fe69bbef097665b3dc0f4a7d86cb6ab4
gitdegsew <abebawdegsew@gmail.com>
2023-07-23 13:06:25 +0300

    footer finished

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,3 +35,4 @@ const Footer = () => (
       </div>
       <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
       <p>@2023 CarHub. All rights reserved</p>
+      <div className="footer__copyrights-link">
