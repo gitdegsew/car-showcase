08f4b55af2cd3441ad76a7c6e438a7abd0a0b5d0
gitdegsew <abebawdegsew@gmail.com>
2023-07-24 13:15:21 +0300

    change navbar width

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,3 +7,4 @@ import CustomButton from "./CustomButton";
 
 const NavBar = () => (
   <header className='w-full  absolute z-10'>
+    <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
