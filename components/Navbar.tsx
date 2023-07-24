0a579138132941f40e2e5e1d9374e3c08c2ab0ae
gitdegsew <abebawdegsew@gmail.com>
2023-07-24 13:14:45 +0300

    change navbar width

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,3 +3,6 @@ import Image from "next/image";
 import CustomButton from "./CustomButton";
 import CustomButton from "./CustomButton";
 import CustomButton from "./CustomButton";
+
+
+const NavBar = () => (
