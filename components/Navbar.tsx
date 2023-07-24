fda2e843a949f7cb789d8932302c2e89d8960afa
gitdegsew <abebawdegsew@gmail.com>
2023-07-24 13:14:20 +0300

    change navbar width

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,3 +2,4 @@ import Link from "next/link";
 import Image from "next/image";
 import CustomButton from "./CustomButton";
 import CustomButton from "./CustomButton";
+import CustomButton from "./CustomButton";
