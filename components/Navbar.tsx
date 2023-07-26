9886979992a8b2d16d8409366a8c683f16168a3c
gitdegsew <abebawdegsew@gmail.com>
2023-07-26 13:26:59 +0300

    modify nav bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,6 @@ import CustomButton from "./CustomButton";
 const NavBar = () => (
   <header className='w-full  absolute z-10'>
     <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
-      <Link href='/' className='flex justify-center items-center'>
         <Image
           alt='logo'
       </Link>
