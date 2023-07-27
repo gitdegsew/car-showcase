aa215d49223cea8c7d66236700f30cd0beed3d01
gitdegsew <abebawdegsew@gmail.com>
2023-07-27 13:34:04 +0300

    search bar icon

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -9,3 +9,4 @@ import { useRouter } from "next/navigation";
 import SearchManufacturer from "./SearchManufacturer";
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
     <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
+  <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
