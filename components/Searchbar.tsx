d07095d91e325cc496889cf4c0c593e19f7f90a4
gitdegsew <abebawdegsew@gmail.com>
2023-07-29 13:43:38 +0300

    set inputs

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -41,3 +41,5 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
     // Create a new URLSearchParams object using the current URL search parameters
     // Create a new URLSearchParams object using the current URL search parameters
     const searchParams = new URLSearchParams(window.location.search);
+
+    // Update or delete the 'model' search parameter based on the 'model' value
