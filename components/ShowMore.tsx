e152d19d85ab197054353e54b709bda18e2d5880
gitdegsew <abebawdegsew@gmail.com>
2023-08-02 14:17:07 +0300

    update limit

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -16,7 +16,6 @@ const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
     // Calculate the new limit based on the page number and navigation type
     const newLimit = (pageNumber + 1) * 10;
 
-    const newPathname = updateSearchParams("limit", `${newLimit}`);
     router.push(newPathname);
 };
 
