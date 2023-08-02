f9c8b30719a37a9fea37da3ffaa771aa95687dea
gitdegsew <abebawdegsew@gmail.com>
2023-08-02 14:11:44 +0300

    router options

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -20,3 +20,4 @@ const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
     const newPathname = updateSearchParams("limit", `${newLimit}`);
     const newPathname = updateSearchParams("limit", `${newLimit}`);
     // Update the "limit" search parameter in the URL with the new value
+    const newPathname = updateSearchParams("limit", `${newLimit}`);
