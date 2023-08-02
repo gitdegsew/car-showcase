4a26ead41d46654522a696d3d2cb211973b24be3
gitdegsew <abebawdegsew@gmail.com>
2023-08-02 14:17:26 +0300

    update limit

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -24,7 +24,6 @@ return (
     <div className="w-full flex-center gap-5 mt-10">
       {!isNext && (
         <CustomButton
-        btnType="button"
         title="Show More"
         containerStyles="bg-primary-blue rounded-full text-white"
         handleClick={handleNavigation}
