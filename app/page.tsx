a9bcd724e3c6c0001001121c63c81299718e5084
gitdegsew <abebawdegsew@gmail.com>
2023-08-04 10:06:23 +0300

    fetchCars

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,13 @@ import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "@components";
 
 
 export default function Home() {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
   return (
     <main className="overflow-hidden">
       <Hero/>
