d014defb8683855f926a77149d66db6d8b74c393
gitdegsew <abebawdegsew@gmail.com>
2023-08-04 10:10:02 +0300

    finish page

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import { HomeProps } from "@types";
 import { fuels, yearsOfProduction } from "@constants";
 import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "@components";
 
-
-export default function Home() {
+export default async function Home({ searchParams }: HomeProps) {
   const allCars = await fetchCars({
     manufacturer: searchParams.manufacturer || "",
     year: searchParams.year || 2022,
@@ -12,14 +11,19 @@ export default function Home() {
     limit: searchParams.limit || 10,
     model: searchParams.model || "",
   });
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
-    <main className="overflow-hidden">
-      <Hero/>
+    <main className='overflow-hidden'>
+      <Hero />
+
       <div className='mt-12 padding-x padding-y max-width' id='discover'>
         <div className='home__text-container'>
           <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
           <p>Explore out cars you might like</p>
         </div>
+
         <div className='home__filters'>
           <SearchBar />
 
@@ -29,8 +33,26 @@ export default function Home() {
           </div>
         </div>
 
+        {!isDataEmpty ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
 
-        </div>
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
+          </section>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
+     
     </main>
-  )
+  );
 }
