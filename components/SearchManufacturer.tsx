0693ef117a39c50a4d91a0b77df01648af82db13
gitdegsew <abebawdegsew@gmail.com>
2023-08-06 14:32:13 +0300

    button for commbox

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -2,3 +2,5 @@ import Image from "next/image";
 import { Fragment, useState } from "react";
 import { Combobox, Transition } from "@headlessui/react";
 import { manufacturers } from "@constants";
+
+import { SearchManuFacturerProps } from "@types";
