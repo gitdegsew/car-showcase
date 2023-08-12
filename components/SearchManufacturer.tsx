9ec191d0962d1af196c9c8adfdcef764f9ce58f7
gitdegsew <abebawdegsew@gmail.com>
2023-08-12 14:39:07 +0300

    button for commbox

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -20,3 +20,8 @@ const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacture
 
   return (
     <div className='search-manufacturer'>
+      <Combobox value={manufacturer} onChange={setManuFacturer}>
+      <div className='relative w-full'>
+          {/* Button for the combobox. Click on the icon to see the complete dropdown */}
+          <Combobox.Button className='absolute top-[14px]'>
+          <Image
