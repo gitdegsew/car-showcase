13a14be8d0f4c5e12327966285eebab7824a0583
gitdegsew <abebawdegsew@gmail.com>
2023-08-14 15:05:02 +0300

    search options

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -58,6 +58,7 @@ const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacture
                 >
                   Create "{query}"
                   </Combobox.Option>
+              ) : (
 
           
            
