026459820c38dd943ddb79c065076d276d66ae74
gitdegsew <abebawdegsew@gmail.com>
2023-08-14 15:15:06 +0300

    search types

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -68,6 +68,7 @@ const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacture
                     }`
                 }
                 value={item}
+                >
 
           
            
