b855436b922ea9a8f4d37c71681d1500f63955c9
gitdegsew <abebawdegsew@gmail.com>
2023-08-14 15:18:03 +0300

    search types

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -88,6 +88,24 @@ const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacture
                           >
            
                 ))
+
+                <Combobox.Button className='absolute top-[14px]'>
+                <Image
+                  src='/car-logo.svg'
+                  width={20}
+                  height={20}
+                  className='ml-4'
+                  alt='car logo'
+                />
+              </Combobox.Button>
+    
+              {/* Input field for searching */}
+              <Combobox.Input
+                className='search-manufacturer__input'
+                displayValue={(item: string) => item}
+                onChange={(event) => setQuery(event.target.value)} // Update the search query when the input changes
+                placeholder='Volkswagen...'
+              />
               )}
            
           </Transition>
