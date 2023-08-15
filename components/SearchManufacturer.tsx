cd794e43d502c7c0807999b33b4c6c7c9008e049
gitdegsew <abebawdegsew@gmail.com>
2023-08-15 15:19:34 +0300

    search types

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -32,12 +32,14 @@ const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacture
               alt='car logo'
             />
           </Combobox.Button>
+
+          {/* Input field for searching */}
           <Combobox.Input
             className='search-manufacturer__input'
             displayValue={(item: string) => item}
             onChange={(event) => setQuery(event.target.value)} // Update the search query when the input changes
             placeholder='Volkswagen...'
-            />
+          />
 
           {/* Transition for displaying the options */}
           <Transition
@@ -46,68 +48,34 @@ const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacture
             leaveFrom='opacity-100'
             leaveTo='opacity-0'
             afterLeave={() => setQuery("")} // Reset the search query after the transition completes
-            >
+          >
             <Combobox.Options
               className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
               static
-              >
+            >
               {filteredManufacturers.length === 0 && query !== "" ? (
                 <Combobox.Option
-                value={query}
-                className='search-manufacturer__option'
+                  value={query}
+                  className='search-manufacturer__option'
                 >
                   Create "{query}"
-                  </Combobox.Option>
+                </Combobox.Option>
               ) : (
                 filteredManufacturers.map((item) => (
-                    <Combobox.Option
+                  <Combobox.Option
                     key={item}
                     className={({ active }) =>
-                    `relative search-manufacturer__option ${
+                      `relative search-manufacturer__option ${
                         active ? "bg-primary-blue text-white" : "text-gray-900"
-                    }`
-                }
-                value={item}
-                >
-                    {({ selected, active }) => (
+                      }`
+                    }
+                    value={item}
+                  >
+                                           {selected ? (
 
-<>
-<span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
-{item}
-
-</span>
-          
-                        {/* Show an active blue background color if the option is selected */}
-                        {selected ? (
-                        {selected ? (
-                            as={Fragment} // group multiple elements without introducing an additional DOM node i.e., <></>
-                            leave='transition ease-in duration-100'
-                            leaveFrom='opacity-100'
-                            leaveTo='opacity-0'
-                            afterLeave={() => setQuery("")} // Reset the search query after the transition completes
-                          >
-           
                 ))
-
-                <Combobox.Button className='absolute top-[14px]'>
-                <Image
-                  src='/car-logo.svg'
-                  width={20}
-                  height={20}
-                  className='ml-4'
-                  alt='car logo'
-                />
-              </Combobox.Button>
-    
-              {/* Input field for searching */}
-              <Combobox.Input
-                className='search-manufacturer__input'
-                displayValue={(item: string) => item}
-                onChange={(event) => setQuery(event.target.value)} // Update the search query when the input changes
-                placeholder='Volkswagen...'
-              />
               )}
-           
+            </Combobox.Options>
           </Transition>
         </div>
       </Combobox>
