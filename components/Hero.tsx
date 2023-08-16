db0d64d1af42b84e12fb44a1300fc093be528093
gitdegsew <abebawdegsew@gmail.com>
2023-08-16 15:26:56 +0300

    nav bar show

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,3 +9,4 @@ const Hero = () => {
   const handleScroll = () => {
     const nextSection = document.getElementById("discover");
     if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
