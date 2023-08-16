ecf82e04da6571f3bebd53bda0ce0c0bfba2fe11
gitdegsew <abebawdegsew@gmail.com>
2023-08-16 15:28:39 +0300

    nav bar show

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,3 +17,4 @@ const Hero = () => {
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
       <h1 className="hero__title">
+      Find, book, rent a car—quick and super easy!
