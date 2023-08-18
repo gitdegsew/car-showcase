1b94b2d68ebcbe80edfa0b79e379ff665afebf15
gitdegsew <abebawdegsew@gmail.com>
2023-08-18 15:40:56 +0300

    car filter

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -61,7 +61,6 @@ const CarCard = ({ car }: CarCardProps) => {
 
 <Combobox.Input
             className='search-manufacturer__input'
-            onChange={(event) => setQuery(event.target.value)} // Update the search query when the input changes
           />
 
 
