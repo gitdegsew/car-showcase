51de2939538993d9a9dc6c4017399d5adf7b764b
gitdegsew <abebawdegsew@gmail.com>
2023-08-18 15:38:15 +0300

    car filter

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,13 @@
 "use client";
-"use client";
+
 import Image from "next/image";
+
 import { CustomButton } from "@components";
-
-
 
 const Hero = () => {
   const handleScroll = () => {
     const nextSection = document.getElementById("discover");
+
     if (nextSection) {
       nextSection.scrollIntoView({ behavior: "smooth" });
     }
@@ -16,38 +16,30 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
-      <h1 className="hero__title">
-      Find, book, rent a car—quick and super easy!
-      </h1>
+        <h1 className="hero__title">
+          Find, book, rent a car—quick and super easy!
+        </h1>
 
-      const handleScroll = () => {
-    const nextSection = document.getElementById("discover");
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          process.
+        </p>
 
-    if (nextSection) {
-      nextSection.scrollIntoView({ behavior: "smooth" });
-    }
-  };
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
 
-const handleScroll = () => {
-    const nextSection = document.getElementById("discover");
+        <div className="hero__image-overlay" />
+      </div>
+    </div>
+  );
+};
 
-    if (nextSection) {
-      nextSection.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
-const handleScroll = () => {
-    const nextSection = document.getElementById("discover");
-
-    if (nextSection) {
-      nextSection.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
-const handleScroll = () => {
-    const nextSection = document.getElementById("discover");
-
-    if (nextSection) {
-      nextSection.scrollIntoView({ behavior: "smooth" });
-    }
-  };
+export default Hero;
