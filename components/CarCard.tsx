68180319bd8965f6dfeb774061aeee0868ce53fe
gitdegsew <abebawdegsew@gmail.com>
2023-08-18 15:39:13 +0300

    car filter

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -50,6 +50,13 @@ const CarCard = ({ car }: CarCardProps) => {
             <Image src="/tire.svg" width={20} height={20} alt="seat" />
             <p className="car-card__icon-text">{drive.toUpperCase()}</p>
           </div>
+          <Combobox.Input
+            className='search-manufacturer__input'
+            displayValue={(item: string) => item}
+            onChange={(event) => setQuery(event.target.value)} // Update the search query when the input changes
+            placeholder='Volkswagen...'
+          />
+
           
           <div className="car-card__icon">
             <Image src="/gas.svg" width={20} height={20} alt="seat" />
