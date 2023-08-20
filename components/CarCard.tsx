38a9e8b50fcaf2d4da72e7e5eade65bced8962be
gitdegsew <abebawdegsew@gmail.com>
2023-08-20 15:54:33 +0300

    adding constants

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -26,41 +26,58 @@ const CarCard = ({ car }: CarCardProps) => {
           {make} {model}
         </h2>
       </div>
-      <button
-    disabled={isDisabled}
-    type={btnType || "button"}
-    className={`custom-btn ${containerStyles}`}
-    onClick={handleClick}
-  >
-      < <button
-    disabled={isDisabled}
-    type={btnType || "button"}
-    className={`custom-btn ${containerStyles}`}
-    onClick={handleClick}
-  >
+
+      <p className='flex mt-6 text-[32px] leading-[38px] font-extrabold'>
+        <span className='self-start text-[14px] leading-[17px] font-semibold'>$</span>
+        {carRent}
+        <span className='self-end text-[14px] leading-[17px] font-medium'>/day</span>
+      </p>
+
+      <div className='relative w-full h-40 my-3 object-contain'>
+        <Image src={generateCarImageUrl(car)} alt='car model' fill priority className='object-contain' />
+      </div>
+
+      <div className='relative flex w-full mt-2'>
+        <div className='flex group-hover:invisible w-full justify-between text-grey'>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image src='/steering-wheel.svg' width={20} height={20} alt='steering wheel' />
+            <p className='text-[14px] leading-[17px]'>
+              {transmission === "a" ? "Automatic" : "Manual"}
+            </p>
+          </div>
+          <Listbox
+        value={selected}
+        onChange={(e) => {
+          setSelected(e); // Update the selected option in state
+          handleUpdateParams(e); // Update the URL search parameters and navigate to the new URL
+        }}
+      >
+
+          <div className="car-card__icon">
+            <Image src="/tire.svg" width={20} height={20} alt="seat" />
+            <p className="car-card__icon-text">{drive.toUpperCase()}</p>
+          </div>
+          
+          <div className="car-card__icon">
+            <Image src="/gas.svg" width={20} height={20} alt="seat" />
+            <p className="car-card__icon-text">{city_mpg} MPG</p>
+          </div>
+        </div>
+
+        <div className="car-card__btn-container">
+          <CustomButton
+            title='View More'
+            containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
+            textStyles='text-white text-[14px] leading-[17px] font-bold'
+            rightIcon='/right-arrow.svg'
+            handleClick={() => setIsOpen(true)}
+          />
+        </div>
+      </div>
+
+      <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car} />
+    </div>
   );
-
-  <button
-    disabled={isDisabled}
-    type={btnType || "button"}
-    className={`custom-btn ${containerStyles}`}
-    onClick={handleClick}
-  >
-
-  <button
-    disabled={isDisabled}
-    type={btnType || "button"}
-    className={`custom-btn ${containerStyles}`}
-    onClick={handleClick}
-  >
-     <Image
-          src={rightIcon}
-          alt="arrow_left"
-          fill
-          className="object-contain"
-        />
-}; 
-
-
+};
 
 export default CarCard;
