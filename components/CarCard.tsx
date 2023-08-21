83356051ff4027ccc0c2e15120865228a73df283
gitdegsew <abebawdegsew@gmail.com>
2023-08-21 15:59:35 +0300

    banner changed

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -12,8 +12,24 @@ import bannerthree from "@/images/bannerthree.jpg";
 import BannerText from "./BannerText";
 import { PiCaretLeftLight, PiCaretRightLight } from "react-icons/pi";
 
-const Banner = () => {
-    const NextArrow = (props: any) => {
-        const NextArrow = (props: any) => {
-            const NextArrow = (props: any) => {
-                const NextArrow = (props: any) => {
+const NextArrow = (props: any) => {
+    const { onClick } = props;
+    return (
+      <div
+        className="p-3 bg-slate-100 hover:text-orange-600 hover:bg-white cursor-pointer duration-200 rounded-full text-2xl flex items-center justify-center z-20 absolute left-2 top-1/2"
+        onClick={onClick}
+      >
+        <PiCaretLeftLight />
+      </div>
+    );
+  };
+  const PrevArrow = (props: any) => {
+    const { onClick } = props;
+    return (
+      <div
+        className="p-3 bg-slate-100 hover:text-orange-600 hover:bg-white cursor-pointer duration-200 rounded-full text-2xl flex items-center justify-center z-20 absolute right-2 top-1/2"
+        onClick={onClick}
+      >
+        <PiCaretRightLight />
+      </div>
+    );
