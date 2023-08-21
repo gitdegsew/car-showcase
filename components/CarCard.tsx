a25999c145bd8fd907da34f3c981fb40c7f8083d
gitdegsew <abebawdegsew@gmail.com>
2023-08-21 15:56:40 +0300

    banner changed

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,3 +3,4 @@ import Slider from "react-slick";
 import bannerone from "@/images/bannerone.jpg";
 import bannertwo from "@/images/bannertwo.jpg";
 import bannertwo from "@/images/bannertwo.jpg";
+import bannertwo from "@/images/bannertwo.jpg";
