4bcf6aa6df6bd2c568f5ab2d933722577c0d8a16
gitdegsew <abebawdegsew@gmail.com>
2023-08-24 16:12:24 +0300

    car price

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -21,46 +21,9 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => (
     <div className="w-full rounded-lg overflow-hidden">
     <Link href={{ pathname: "/product", query: { _id: item?._id } }}>
     <Link href={{ pathname: "/product", query: { _id: item?._id } }}>
-    <Image
-              src={item?.image}
-              alt="product image"
-              width={500}
-              height={500}
-              className="w-full h-full object-cover group-hover:scale-110 duration-200 rounded-t-lg"
-            />
-             <Image
-              src={item?.image}
-              alt="product image"
-              width={500}
-              height={500}
-              className="w-full h-full object-cover group-hover:scale-110 duration-200 rounded-t-lg"
-            />
-
-<Image
-              src={item?.image}
-              alt="product image"
-              width={500}
-              height={500}
-              className="w-full h-full object-cover group-hover:scale-110 duration-200 rounded-t-lg"
-            />
-      {item?.isNew && (
-              <span className="absolute top-2 right-2 font-medium text-xs py-1 px-3 rounded-full bg-white group-hover:bg-orange-600 group-hover:text-white duration-200">
-                New Arrival
-              </span>
-            )}
-
-{item?.isNew && (
-              <span className="absolute top-2 right-2 font-medium text-xs py-1 px-3 rounded-full bg-white group-hover:bg-orange-600 group-hover:text-white duration-200">
-                New Arrival
-              </span>
-            )}
-
-{item?.isNew && (
-              <span className="absolute top-2 right-2 font-medium text-xs py-1 px-3 rounded-full bg-white group-hover:bg-orange-600 group-hover:text-white duration-200">
-                New Arrival
-              </span>
-            )}
-);
-const startArray = Array.from({ length: item?.rating }, (_, index) => (
+  
+    <div className="border-[1px] border-orange-600 py-1 px-4 rounded-full text-xs">
+              <p>{calculatePercentage(item?.price, item?.oldPrice)}% off</p>
+            </div>
 
 export default CarDetails;
