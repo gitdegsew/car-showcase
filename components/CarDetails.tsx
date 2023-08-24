29d0e169928ddf8b8bdc2c7cad45ad6f3786b02c
gitdegsew <abebawdegsew@gmail.com>
2023-08-24 16:11:40 +0300

    car price

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -54,6 +54,12 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => (
                 New Arrival
               </span>
             )}
+
+{item?.isNew && (
+              <span className="absolute top-2 right-2 font-medium text-xs py-1 px-3 rounded-full bg-white group-hover:bg-orange-600 group-hover:text-white duration-200">
+                New Arrival
+              </span>
+            )}
 );
 const startArray = Array.from({ length: item?.rating }, (_, index) => (
 
