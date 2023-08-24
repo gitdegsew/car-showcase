3d9ab474c958151584fd7a4d249a0f74a7c027ea
gitdegsew <abebawdegsew@gmail.com>
2023-08-24 16:09:51 +0300

    car price

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -19,6 +19,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => (
     <div className="w-full rounded-lg overflow-hidden">
 
     <div className="w-full rounded-lg overflow-hidden">
+    <Link href={{ pathname: "/product", query: { _id: item?._id } }}>
 
       
 );
