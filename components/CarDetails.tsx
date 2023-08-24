7ccdcb2087720f6b0263c20258e53d3c11efbe75
gitdegsew <abebawdegsew@gmail.com>
2023-08-24 16:12:43 +0300

    car price

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -28,5 +28,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => (
             <div className="border-[1px] border-orange-600 py-1 px-4 rounded-full text-xs">
               <p>{calculatePercentage(item?.price, item?.oldPrice)}% off</p>
             </div>
-
+            <div className="border-[1px] border-orange-600 py-1 px-4 rounded-full text-xs">
+              <p>{calculatePercentage(item?.price, item?.oldPrice)}% off</p>
+            </div>
 export default CarDetails;
