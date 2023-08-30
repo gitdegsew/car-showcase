209684d661c9dc2dc1abf1dc07e44cd887fc8283
gitdegsew <abebawdegsew@gmail.com>
2023-08-30 16:36:09 +0300

    search package

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -27,7 +27,13 @@ const NavBar = () => (
                   <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg'>
                     <Image src={generateCarImageUrl(car)} alt='car model' fill priority className='object-contain' />
                   </div>
-
+                  <Image
+                    src='/close.svg'
+                    alt='close'
+                    width={20}
+                    height={20}
+                    className='object-contain'
+                  />
 
 
 
