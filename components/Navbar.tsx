31c33cd30b84240a54a72566dd908055e36a82ce
gitdegsew <abebawdegsew@gmail.com>
2023-08-30 16:35:38 +0300

    search package

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,6 +20,11 @@ const NavBar = () => (
                   </div>
 
 
+                  <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg'>
+                    <Image src={generateCarImageUrl(car)} alt='car model' fill priority className='object-contain' />
+                  </div>
+
+
 
 
       <CustomButton
