a1454f83f88cb524ec460816ca117ed4cbcec054
gitdegsew <abebawdegsew@gmail.com>
2023-09-01 16:57:44 +0300

    hero filter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -21,6 +21,7 @@ const Hero = () => {
       </h1>
       <p className="hero__subtitle">
       Streamline your car rental experience with our effortless booking
+      process.
       
     </div>
   );
