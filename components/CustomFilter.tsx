e85f401d13e7f47a29b6d7987f490d7fa3e3a746
gitdegsew <abebawdegsew@gmail.com>
2023-09-01 16:59:36 +0300

    hero filter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -27,6 +27,7 @@ const Hero = () => {
           title="Explore Cars"
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
+          />
 
       
     </div>
