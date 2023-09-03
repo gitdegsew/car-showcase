f663586cd68569673df0e2989cd6efbf135b32d8
gitdegsew <abebawdegsew@gmail.com>
2023-09-03 17:58:15 +0300

    change the bg

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,6 +13,17 @@ import Link from "next/link";
 import Image from "next/image";
 
 const Footer = () => {
+
+    const router = useRouter();
+
+  const handleNavigation = () => {
+    // Calculate the new limit based on the page number and navigation type
+    const newLimit = (pageNumber + 1) * 10;
+
+    // Update the "limit" search parameter in the URL with the new value
+    const newPathname = updateSearchParams("limit", `${newLimit}`);
+    
+    router.push(newPathname);
     return (
     <div className="w-full flex-center gap-5 mt-10">
       {!isNext && (
