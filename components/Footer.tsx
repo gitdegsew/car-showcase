9cab6d804e9b482a0532352c0ecd9520361cdda6
gitdegsew <abebawdegsew@gmail.com>
2023-09-03 17:58:54 +0300

    change the bg

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,7 +20,6 @@ const Footer = () => {
     // Calculate the new limit based on the page number and navigation type
     const newLimit = (pageNumber + 1) * 10;
 
-    // Update the "limit" search parameter in the URL with the new value
     const newPathname = updateSearchParams("limit", `${newLimit}`);
     
     return (
