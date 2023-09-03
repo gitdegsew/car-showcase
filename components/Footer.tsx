9e51d695eb9a69a38dd2ffebe4d0e548d6d93074
gitdegsew <abebawdegsew@gmail.com>
2023-09-03 17:58:41 +0300

    change the bg

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -23,7 +23,6 @@ const Footer = () => {
     // Update the "limit" search parameter in the URL with the new value
     const newPathname = updateSearchParams("limit", `${newLimit}`);
     
-    router.push(newPathname);
     return (
     <div className="w-full flex-center gap-5 mt-10">
       {!isNext && (
