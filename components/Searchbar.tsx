fe67dafff7bb9107895406f707d4a571402cb9f7
gitdegsew <abebawdegsew@gmail.com>
2023-09-04 18:03:14 +0300

    logo modified

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -62,6 +62,7 @@ const SearchBar = () => {
     <form className='searchbar' onSubmit={handleSearch}>
 
     <div className="hidden lg:inline-block absolute top-0 left-0 w-full h-full">
+    <Container className="flex h-full flex-col gap-y-6 justify-center">
 
       <div className='searchbar__item'>
         <SearchManufacturer
