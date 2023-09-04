b7a3faa1cd45341333c718535ece7cb7a56d9470
gitdegsew <abebawdegsew@gmail.com>
2023-09-04 18:13:12 +0300

    layout bug

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -81,6 +81,7 @@ const SearchBar = () => {
         />
 
         <div className="border-[1px] border-slate-300 border-t-0 px-2 py-4 flex flex-col gap-y-2 bg-white rounded-b-lg">
+        <div className="flex items-center justify-between">
 
         
       </div>
