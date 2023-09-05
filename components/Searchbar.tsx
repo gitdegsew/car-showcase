0e3415f4082976c865db04f95495c7e1b076c1b9
gitdegsew <abebawdegsew@gmail.com>
2023-09-05 18:22:39 +0300

    remove page error

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -95,7 +95,6 @@ const SearchBar = () => {
                 <FormattedPrice amount={item?.oldPrice} />
               </p><p className="text-slate-500 line-through text-sm">
                 <FormattedPrice amount={item?.oldPrice} />
-              </p>
 
                 <FormattedPrice amount={item?.oldPrice} />
               </p>
