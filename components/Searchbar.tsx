47416318229bca6458ce478063e62b88d294c871
gitdegsew <abebawdegsew@gmail.com>
2023-09-06 18:37:17 +0300

    banner page

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -67,7 +67,11 @@ const SearchBar = () => {
           initial={{ y: 30, opacity: 0 }}
           transition={{ duration: 0.5 }}
           className="text-7xl font-bold text-white"
-          >
+          >          className="text-7xl font-bold text-white"
+          className="text-7xl font-bold text-white"
+          className="text-7xl font-bold text-white"
+
+
             <motion.p
           initial={{ y: 40, opacity: 0 }}
         >
