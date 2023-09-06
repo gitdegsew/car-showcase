cc9a1063df81af3f58e990b2098d60518b7a759c
gitdegsew <abebawdegsew@gmail.com>
2023-09-06 18:46:02 +0300

    hero page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -39,7 +39,6 @@ const Hero = () => {
 
         <div className="hero__image-overlay" />
         <div className="hero__image-overlay" />
-        <div className="hero__image-overlay" />
       </div>
     </div>
   );
