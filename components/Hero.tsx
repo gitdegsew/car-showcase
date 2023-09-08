b4386a3ea2f087f79d19601c92fe336c53d502a7
gitdegsew <abebawdegsew@gmail.com>
2023-09-08 18:59:51 +0300

    container layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -37,7 +37,6 @@ const Hero = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
 
       </div>
