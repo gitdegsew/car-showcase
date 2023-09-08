2b397ba96724fd4a6359ff3f6e660c12592e38c8
gitdegsew <abebawdegsew@gmail.com>
2023-09-08 19:02:15 +0300

    container layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,7 +21,6 @@ const Hero = () => {
         </h1>
 
         <p className="hero__subtitle">
-          Streamline your car rental experience with our effortless booking
           process.
 
         <CustomButton
