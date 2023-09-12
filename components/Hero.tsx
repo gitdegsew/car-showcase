242ef1b488c6f39bbddb51e7eb412127709d52bb
gitdegsew <abebawdegsew@gmail.com>
2023-09-12 19:12:16 +0300

    not found error

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -34,6 +34,9 @@ const Hero = () => {
             <p className="font-semibold">
             <FormattedPrice amount={item?.price} />
           </p>
+          <p className="font-semibold">
+          <FormattedPrice amount={item?.price} />
+        </p>
 
 
 
