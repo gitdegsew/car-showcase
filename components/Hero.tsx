6f251ef2af5e2f5228b7b0b7e19a8d945e274857
gitdegsew <abebawdegsew@gmail.com>
2023-09-13 19:15:44 +0300

    format price

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -38,7 +38,6 @@ const Hero = () => {
           <FormattedPrice amount={item?.price} />
         </p>
         <p className="font-semibold">
-        <FormattedPrice amount={item?.price} />
 
 <p className="font-semibold">
               </p>
