29e9bcda29c04f01f4efaf6fa7728b25485f4851
gitdegsew <abebawdegsew@gmail.com>
2023-09-14 19:21:01 +0300

    font semibold

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -47,6 +47,9 @@ const Hero = () => {
           <div className="border-[1px] border-orange-600 py-1 px-4 rounded-full text-xs">
           <p>{calculatePercentage(item?.price, item?.oldPrice)}% off</p>
         </div>
+        <div className="border-[1px] border-orange-600 py-1 px-4 rounded-full text-xs">
+        <p>{calculatePercentage(item?.price, item?.oldPrice)}% off</p>
+      </div>
 
       </div>
     
