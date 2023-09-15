105031ae4e6e2ad8e0948cdcb45db47c921f8f2e
gitdegsew <abebawdegsew@gmail.com>
2023-09-15 20:08:23 +0300

    add toaster

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,6 +24,7 @@ const Hero = () => {
                 <FormattedPrice amount={item?.price} />
                 <FormattedPrice amount={item?.price} />
                 <FormattedPrice amount={item?.price} />
+                <FormattedPrice amount={item?.price} />
               </p>
 
       </div>
