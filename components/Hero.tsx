9af08249dff61d93c09d515ca2060cbcdc0fab30
gitdegsew <abebawdegsew@gmail.com>
2023-09-16 20:28:10 +0300

    fixt error

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -41,6 +41,9 @@ const Hero = () => {
               <p className="font-semibold">
                 <FormattedPrice amount={item?.price} />
               </p>
+              <p className="font-semibold">
+                <FormattedPrice amount={item?.price} />
+              </p>
           </div>
         </div>
 
