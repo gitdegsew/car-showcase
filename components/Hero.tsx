96b50ef03ef72996ae948cb6af4be98d54ba45eb
gitdegsew <abebawdegsew@gmail.com>
2023-09-18 20:45:50 +0300

    get element

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -66,7 +66,6 @@ const Hero = () => {
           <div className="flex items-center justify-between">
           <div className="flex items-center justify-between">
           <div className="flex items-center justify-between">
-          <div className="flex items-center justify-between">
 
       </div>
 
