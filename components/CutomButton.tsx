2ab3f4f9d5970964bf62ca46c6cba02035665537
gitdegsew <abebawdegsew@gmail.com>
2023-09-19 20:54:03 +0300

    get object

--- components/CutomButton.tsx
+++ components/CutomButton.tsx
@@ -14,6 +14,18 @@ const Button = ({ isDisabled, btnType, containerStyles, textStyles, title, right
     {rightIcon && (
       <div className="relative w-6 h-6">
         <Image
+        <div className="flex items-center justify-between">
+        <div className="border-[1px] border-orange-600 py-1 px-4 rounded-full text-xs">
+          <p>{calculatePercentage(item?.price, item?.oldPrice)}% off</p>
+        </div>
+        <div className="flex items-center gap-x-2">
+          <p className="text-slate-500 line-through text-sm">
+            <FormattedPrice amount={item?.oldPrice} />
+          </p>
+          <p className="font-semibold">
+            <FormattedPrice amount={item?.price} />
+          </p>
+        </div>
     )}
   </button>
 export default Button;
