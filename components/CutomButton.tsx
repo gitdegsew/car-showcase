02fb98295ba740b9ba6fbbdb6918ad60906c4936
gitdegsew <abebawdegsew@gmail.com>
2023-09-19 20:55:18 +0300

    get object

--- components/CutomButton.tsx
+++ components/CutomButton.tsx
@@ -15,7 +15,6 @@ const Button = ({ isDisabled, btnType, containerStyles, textStyles, title, right
       <div className="relative w-6 h-6">
         <Image
         <div className="flex items-center justify-between">
-        <div className="border-[1px] border-orange-600 py-1 px-4 rounded-full text-xs">
           <p>{calculatePercentage(item?.price, item?.oldPrice)}% off</p>
         </div>
           </p>
