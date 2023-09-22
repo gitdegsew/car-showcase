4c01d32c7d53237b43af0aefaa86abbad117f1c0
gitdegsew <abebawdegsew@gmail.com>
2023-09-22 21:11:00 +0300

    get object

--- components/CutomButton.tsx
+++ components/CutomButton.tsx
@@ -23,7 +23,6 @@ const Button = ({ isDisabled, btnType, containerStyles, textStyles, title, right
     <span className={`flex-1 ${textStyles}`}>{title}</span>
         </div>
           </p>
-        <div className="flex items-center justify-between">
         </div>
     )}
   </button>
