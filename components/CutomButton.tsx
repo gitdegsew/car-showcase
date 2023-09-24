ff5c82cf39e784faedd775027a0bdb861944cb90
gitdegsew <abebawdegsew@gmail.com>
2023-09-24 21:15:41 +0300

    change title

--- components/CutomButton.tsx
+++ components/CutomButton.tsx
@@ -16,7 +16,6 @@ const Button = ({ isDisabled, btnType, containerStyles, textStyles, title, right
     <div className="border-[1px] border-orange-600 py-1 px-4 rounded-full text-xs">
       <p>{calculatePercentage(item?.price, item?.oldPrice)}% off</p>
     </div>
-    <div className="flex items-center gap-x-2">
       <p className="text-slate-500 line-through text-sm">
       </p>
       <p className="font-semibold">
