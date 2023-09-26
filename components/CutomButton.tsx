0be6461293a3ce3ab774914668f35cf142703ef6
gitdegsew <abebawdegsew@gmail.com>
2023-09-26 21:21:06 +0300

    dispatch cart

--- components/CutomButton.tsx
+++ components/CutomButton.tsx
@@ -37,7 +37,6 @@ const Button = ({ isDisabled, btnType, containerStyles, textStyles, title, right
 {/* add to cart button */}
 <button
   // onClick={() =>
-  //   dispatch(addToCart(item)) &&
   //   )
   // }
   className="bg-orange-600 px-4 py-2 text-sm tracking-wide rounded-full text-slate-100 hover:bg-orange-800 hover:text-white duration-200"
