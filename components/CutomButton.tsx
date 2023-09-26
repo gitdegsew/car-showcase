ef967a6301db9d94dced9e9e154cc271538ac413
gitdegsew <abebawdegsew@gmail.com>
2023-09-26 21:19:46 +0300

    dispatch cart

--- components/CutomButton.tsx
+++ components/CutomButton.tsx
@@ -23,5 +23,34 @@ const Button = ({ isDisabled, btnType, containerStyles, textStyles, title, right
       </p>
     </div>
 
+</div>
+<div className="flex items-center gap-x-2">
+  <p className="text-slate-500 line-through text-sm">
+    <FormattedPrice amount={item?.oldPrice} />
+  </p>
+  <p className="font-semibold">
+    <FormattedPrice amount={item?.price} />
+  </p>
+</div>
+</div>
+<div className="flex items-center justify-between">
+{/* add to cart button */}
+<button
+  // onClick={() =>
+  //   dispatch(addToCart(item)) &&
+  //   toast.success(
+  //     `${item?.title.substring(0, 15)} added successfully!`
+  //   )
+  // }
+  className="bg-orange-600 px-4 py-2 text-sm tracking-wide rounded-full text-slate-100 hover:bg-orange-800 hover:text-white duration-200"
+>
+  add to cart
+</button>
+{/* star icons */}
+<div className="flex items-center gap-x-1">{startArray}</div>
+</div>
+</div>
+</div>
+
 
 
