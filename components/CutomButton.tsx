dad2efdcfd195a741aaf2bd8401a3df0bd61ef37
gitdegsew <abebawdegsew@gmail.com>
2023-09-26 21:21:16 +0300

    dispatch cart

--- components/CutomButton.tsx
+++ components/CutomButton.tsx
@@ -39,8 +39,7 @@ const Button = ({ isDisabled, btnType, containerStyles, textStyles, title, right
   // onClick={() =>
   //   )
   // }
-  className="bg-orange-600 px-4 py-2 text-sm tracking-wide rounded-full text-slate-100 hover:bg-orange-800 hover:text-white duration-200"
->
+s>
 {/* star icons */}
 </div>
 </div>
