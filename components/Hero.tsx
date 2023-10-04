4ee66a8f831e1e5b6a20003f35d46d58acc22ca1
gitdegsew <abebawdegsew@gmail.com>
2023-10-04 21:30:51 +0300

    list view

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -62,7 +62,6 @@ const Hero = () => {
             </li>
             <li className="flex flex-col">
               <span className="text-slate hover:text-orange-600 cursor-pointer duration-200">
-                Where Music Is Headed Next
               </span>
             </li>
           </ul>
