4014e02b78754bfb6ce7866958be88a2909db97b
gitdegsew <abebawdegsew@gmail.com>
2023-10-05 21:39:01 +0300

    banner image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,7 +31,6 @@ const Hero = () => {
               </p>
             </div>
               </p>
-              <p className="font-semibold">
               </p>
 
               <p className="font-semibold">
