4580c229731e5cea4ef11ad12ffc95f0db0256dd
gitdegsew <abebawdegsew@gmail.com>
2023-10-08 21:52:32 +0300

    change text color

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 
 
 const NavBar = () => (
-    <header className='w-full  absolute z-10'>
     <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
     <Image
           src='/logo.svg'
