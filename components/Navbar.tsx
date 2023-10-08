73559354ae08e95a880dbe5fde67a7a1ee3e56c9
gitdegsew <abebawdegsew@gmail.com>
2023-10-08 21:52:20 +0300

    change text color

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 const NavBar = () => (
     <header className='w-full  absolute z-10'>
     <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
-    <Link href='/' className='flex justify-center items-center'>
     <Image
           src='/logo.svg'
         />
@@ -20,7 +19,6 @@ const NavBar = () => (
       <CustomButton
         title='Sign in'
         btnType='button'
-        containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
       />
     </nav>
     </header>
