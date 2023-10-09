067b16ee5fcb8aec882b08c30ef8e6c58baa158e
gitdegsew <abebawdegsew@gmail.com>
2023-10-09 21:55:57 +0300

    session data

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -23,7 +23,6 @@ const NavBar = () => (
       }
       {/* Cart button */}
       <div className='bg-black hover:bg-slate-950 rounded-full text-slate-100 hover:text-white flex items-center justify-center gap-x-1 px-3 py-1.5 border-[1px] border-black hover:border-orange-600 duration-200 relative' >
-          <span className='bg-white text-xs text-orange-600 rounded-full font-semibold absolute -right-2 -top-1 w-5 h-5 flex items-center justify-center shadow-xl shadow-black' >
               0
 
           </span>
